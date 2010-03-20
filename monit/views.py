67c728c2b610defe1d150255914f84053baac66c
John Paulett <john@paulett.org>
2010-03-20 00:49:50 -0500

    Add basic logging with Python's standard logger.

--- monit/views.py
+++ monit/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 from monit.models import collect
+import logging
 
 #FIXME add auth & perms
 def collector(request):
     #TODO need to dig more into the monit collector protocol
-    
+
     # only allow POSTs
     if not request.POST:
         return HttpResponseNotAllowed(['POST'])
@@ -14,5 +15,6 @@ def collector(request):
     if data is None or len(data) == 0:
         return HttpResponseBadRequest('need some data')
 
+    logging.info('monit collecter processing data from %s' % request.META['REMOTE_ADDR'])
     collect(data)
     return HttpResponse('ok')
